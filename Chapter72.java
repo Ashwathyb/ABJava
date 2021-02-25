e8806227b89f073e40fd8dca6f331c7a6ec13eae
Ashwathyb <69999325+Ashwathyb@users.noreply.github.com>
2021-02-25 15:02:50 +0800

    Work Stuff

--- Chapter72.java
+++ Chapter72.java
@@ -2,6 +2,22 @@ public class Chapter72 {
 	
 	public static void main (String[] args) {
 		
+		int a = 10;
+		double bb = 1;
+		for ( int i = 1; i < 10; i ++) {
+			bb = bb * 3.732;
+			String x = pad (bb, a);
+			System.out.println (x);
+		}
+		
+	}
+	
+	static String pad (double number, int width) {
+		String aligned = " " + number;
+		while ( aligned.indexOf ('.') < width ) {
+			aligned = " " + aligned;
+		}
+		return aligned;
 	}
 }
 
